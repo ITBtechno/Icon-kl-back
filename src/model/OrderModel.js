e0b1639eca061dadf30da03f5a9545a075d569de
jfrlysona <jfrlysona@gmail.com>
2024-08-06 19:06:28 +0400

    order model updated

--- src/model/OrderModel.js
+++ src/model/OrderModel.js
@@ -5,14 +5,12 @@ const ordersSchema = new Schema(
   {
     amount: { type: Number, required: true },
     status: { type: String, required: true, default: "pending" },
-    promocode: [
-      {
-        code: { type: String, required: true },
-        discount: { type: Number, required: true },
-        expirationDate: { type: Date, required: true },
-        limit: { type: Number, default: 0, ref: "users" },
-      },
-    ],
+    promocode: {
+      code: { type: String, required: true },
+      discount: { type: Number, required: true },
+      expirationDate: { type: Date, required: true },
+      limit: { type: Number, default: 0, ref: "users" },
+    },
     paymentMethod: { type: String, required: true },
     items: [
       {
